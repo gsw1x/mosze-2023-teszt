22721ecb160a92d4e58436afb8f0f152ff948fe1
gsw1x <125861600+gsw1x@users.noreply.github.com>
2023-09-23 14:09:36 +0200

    Javított code

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Rosszul van írva változó név
-    std::cout << '1-100 ertekek duplazasa' // Nincs ';' jel a végén és aposztróf helyett idézőjel
-    for (int i = 0;) //for ciklusban feltételek hiányoznak
+    int *b = new int[N_ELEMENTS]; //Változó név rosszul van írva
+    std::cout << "1-100 ertekek duplazasa"; // Nincs ';' jel a végén és aposztróf helyett idézőjel
+    for (int i = 0; i<N_ELEMENTS; i++) //for ciklusban feltételek hiányoznak
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // for ciklusban hiányzo feltétel
+    for (int i = 0; i<N_ELEMENTS; i++) // for ciklusban hiányzo feltétel
     {
-        std::cout << "Ertek:" //Érték megadása hiányzik
+        std::cout << "Ertek:" << b[i] << std::endl; //Érték megadása hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // Nincs kezdőérték
-    for (int i = 0; i < N_ELEMENTS, i++) // ',' helyett ';' jell kell
+    int atlag=0; // Nincs kezdőérték
+    for (int i = 0; i < N_ELEMENTS; i++) // ',' helyett ';' jell kell
     {
-        atlag += b[i] //Sorvégéről jiányzik a ';'
+        atlag += b[i]; //Sorvégéről jiányzik a ';'
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    // delete [] b hiányzik
+    delete [] b; //Hiányzik a delete [] b
     return 0;
 }
