b3b0216cd18d44f48c739e3946d24b4b277eee96
gsw1x <125861600+gsw1x@users.noreply.github.com>
2023-09-23 14:05:20 +0200

    Hibák leírása

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //Rosszul van írva változó név
+    std::cout << '1-100 ertekek duplazasa' // Nincs ';' jel a végén és aposztróf helyett idézőjel
+    for (int i = 0;) //for ciklusban feltételek hiányoznak
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // for ciklusban hiányzo feltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Érték megadása hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // Nincs kezdőérték
+    for (int i = 0; i < N_ELEMENTS, i++) // ',' helyett ';' jell kell
     {
-        atlag += b[i]
+        atlag += b[i] //Sorvégéről jiányzik a ';'
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // delete [] b hiányzik
     return 0;
 }
